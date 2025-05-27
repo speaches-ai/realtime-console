95e21a86eb63a1bb356431f3676fd0d2ad355149
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2025-05-26 17:47:10 -0700

    chore: update port

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5777,
+  },
   plugins: [react(), tailwindcss()],
 });
